3adad1caa16774499648743f480de72e84646186
Gian Marco Orlando <gian.orlando@studenti.unina.it>
2018-03-21 19:53:00 +0100

    Completed animations of the car.

--- SavEnvironment.playground/Pages/Second Page.xcplaygroundpage/Contents.swift
+++ SavEnvironment.playground/Pages/Second Page.xcplaygroundpage/Contents.swift
@@ -13,6 +13,8 @@ let upLeftImage = UIImage(named: "Street.jpeg")
 
 class backgroundImage: UIImageView {
     var dimension: CGRect = backgroundImageRect
+
+    //SE SERVE POSSO DIVIDERMELE IN PIù PROPRIETà
     //let width = UIScreen.main.bounds.width / 3
 
     convenience init() {
@@ -20,16 +22,19 @@ class backgroundImage: UIImageView {
     }
 }
 
-let carImage = UIImageView(image: car)
-carImage.frame.size = CGSize(width: 90, height: 90)
 
 let backgroundImageUp = backgroundImage()
 let backgroundImageUpLeft = backgroundImage()
 
+let carImage = UIImageView(image: car)
+carImage.frame.size = CGSize(width: 90, height: 90)
+carImage.frame.origin.x = backgroundImageUpLeft.frame.midX - carImage.frame.width / 2
+carImage.frame.origin.y = backgroundImageUpLeft.frame.midY - carImage.frame.height / 2
+
 backgroundImageUp.image = upImage
-backgroundImageUp.contentMode = .scaleToFill
+backgroundImageUp.contentMode = .scaleAspectFit
 backgroundImageUpLeft.image = upLeftImage
-backgroundImageUpLeft.contentMode = .scaleToFill
+backgroundImageUpLeft.contentMode = .scaleAspectFit
 
 view.insertSubview(backgroundImageUpLeft, at: 0)
 view.insertSubview(backgroundImageUp, at: 0)
@@ -42,6 +47,8 @@ backgroundImageUp.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive
 backgroundImageUp.leadingAnchor.constraint(equalTo: backgroundImageUpLeft.trailingAnchor).isActive = true
 backgroundImageUp.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
 
+
+//SE NON FUNZIONANO LE CONSTRAINTS IN NESSUN MODO
 //backgroundImageUp.frame = CGRect(x: backgroundImageUpLeft.frame.maxX, y: 0, width: UIScreen.main.bounds.width / 3, height: UIScreen.main.bounds.height / 3)
 
 func boostingAnimation() {
@@ -54,9 +61,52 @@ func boostingAnimation() {
     }
 }
 
-boostingAnimation()
+func moveRight() {
 
-PlaygroundPage.current.liveView = view
+    UIImageView.animate(withDuration: 2, animations: {
+        carImage.frame.origin.x += backgroundImageRect.width
+    }) { (true) in
+boostingAnimation()
+    }
+}
+
+func moveLeft() {
+
+    UIImageView.animate(withDuration: 2, animations: {
+        carImage.frame.origin.x -= backgroundImageRect.width
+    }) { (true) in
+        boostingAnimation()
+    }
+}
+
+func moveUp() {
+
+    UIImageView.animate(withDuration: 2, animations: {
+        carImage.frame.origin.y -= backgroundImageRect.height
+    }) { (true) in
+        boostingAnimation()
+    }
+}
+
+func moveDown() {
+
+    UIImageView.animate(withDuration: 2, animations: {
+        carImage.frame.origin.y += backgroundImageRect.height
+    }) { (true) in
+        boostingAnimation()
+    }
+}
+
+//COME CAZZO LE SINCRONIZZO ORA?
+moveRight()
+//moveDown()
+//moveUp()
+//moveLeft()
+
+
+view.translatesAutoresizingMaskIntoConstraints = false
 PlaygroundPage.current.needsIndefiniteExecution = true
+PlaygroundPage.current.liveView = view
+
 
 //: [Next](@next)
